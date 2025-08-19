c86ed8691f1d8e3972e19568ac3bab29f422df97
nyaklollen <nykloln7@gmail.com>
2025-08-19 18:12:41 +0530

    Madlibs game

---
+++ Madlibs game/Madlibs-game.py
@@ -0,0 +1,14 @@
+#madlibs game
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun(animal name): ")
+adjective2 = input("Enter an adjective(How was the animal): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective(how you felt): ")
+
+noun1 = noun1.title()
+
+print(f"Today I went to a {adjective1} zoo. ")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
